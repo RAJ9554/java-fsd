eceb6422f35764ea8f2b25399085ec2e7cef18af
raj <rajkumaryadav212601@gmail.com>
2023-05-12 16:26:35 +0530

    dia

--- Practise Project(2)/src/diamond/Diamondpro.java
+++ Practise Project(2)/src/diamond/Diamondpro.java
@@ -1,5 +1,32 @@
 package diamond;
+//Write a program in Java to resolve the diamond problem using OOPs’ concepts
 
-public class Diamondpro {
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class Diamondpro implements First, Second  {
+	   public void show() 
+	    {  
+	        First.super.show(); 
+	        Second.super.show(); 
+	    } 
+	    public static void main(String args[]) 
+	    { 
+	    	Diamondpro  ob = new Diamondpro (); 
+	        ob.show(); 
+	    } 
+	
 
 }
+//yes visible
