9ecf2cd5476cf0ba96260712bb0eeb3b86d6788b
raj <rajkumaryadav212601@gmail.com>
2023-07-19 15:50:13 +0530

    angular-firstprogram

---
+++ agularfirstprogram/src/app/product-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'product-list',
+    templateUrl:'./product-list.component.html'
+})
+export class ProductListComponent{
+    pageTitle: string ="product list page"
+}
